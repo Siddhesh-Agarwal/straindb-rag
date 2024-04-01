ea7a2d03229c828ff48e4b7684cc787b838816c6
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-04-01 16:42:22 +0530

    connected openai llm to retriever

--- st_app.py
+++ st_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-from langchain.vectorstores.chroma import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.vectorstores.chroma import Chroma
+from langchain_core.output_parsers.string import StrOutputParser
+from langchain_core.runnables.passthrough import RunnablePassthrough
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
 
 load_dotenv()
 
@@ -9,21 +13,37 @@ st.title("StrainDB RAG")
 
 
 @st.cache_resource
-def get_vectorstore():
+def get_retriever():
+    """A Chroma retriever that uses OpenAI embeddings"""
+
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     return Chroma(
         collection_name="strains",
         embedding_function=embeddings,
         persist_directory="./chroma/",
-    )
+    ).as_retriever()
 
 
 query = st.text_input("Enter your query")
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+retriever = get_retriever()
+model = ChatOpenAI(temperature=0)
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
 
 if st.button("Search"):
     if not query:
         st.warning("Please enter a query")
     else:
-        vectorstore = get_vectorstore()
-        docs = vectorstore.similarity_search(query, k=3)
-        st.json([doc.json() for doc in docs])
+        with st.spinner("Searching..."):
+            res = chain.stream(query)
+            st.write_stream(res)
